d4cba8903c697729e48226f1fd3a86d7fe2a9fd2
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2022-03-06 23:00:39 -0500

    Se ldibuja el ahorcado, aunque aún no es funcional

--- ahorcado.cpp
+++ ahorcado.cpp
@@ -11,14 +11,11 @@ using namespace std;
 string convertiratexto(float f);
 
 string rellenartexto(string texto_inicial, int longitud_final);
-void imprimirlibros(string matriz[3][4]);
-void imprimirtrabajadores(string matriz[3][5]);
 void presionarteclacontinuar();
 void borrarpantalla();
 void dibujarasteriscos(float longitud_texto);
-void creartitulo(string texto);
-void reglibros();
-void regtrabajadores();
+void CrearTitulo(string texto);
+void MostrarAhorcado(string palabra_jugar, int TotalIntentos,  int IntentosRestantes);
 void menuprincipal();
 string *LeerArchivo();
 
@@ -40,11 +37,12 @@ void IniciarJuego(string palabra)
 
 	for (std::string::size_type i = 0; i < palabra.size(); ++i)
 	{
-		int random = 1 + (rand() % 100);
+		int random = 1 + (rand() % 1000);
+		palabra[i] = toupper(palabra[i]);
 
 		if (random % 2)
 		{
-			palabra_jugar.push_back(palabra[i]);
+			palabra_jugar.push_back(toupper(palabra[i]));
 		}
 		else
 		{
@@ -52,25 +50,25 @@ void IniciarJuego(string palabra)
 			LetrasEscondidas++;
 		}
 	}
-	//	cout << "Palabra 1: " << palabra << " Palabra 2: " << palabra_jugar << endl;
 
 	char caracter = ' ';
 	bool FinJuego = false;
 	int Intentos = LetrasEscondidas;
 
-	cout << "Palabra: " << palabra_jugar << " Intentos " << Intentos << endl;
+	MostrarAhorcado(palabra_jugar, LetrasEscondidas, Intentos);
 
 	while (FinJuego == false)
 	{
+
 		cout << "Ingrese una letra: ";
 		cin >> caracter;
 		cout << endl;
 		bool letra_encontrada = false;
 		for (std::string::size_type i = 0; i < palabra.size(); ++i)
 		{
-			if (palabra[i] == caracter)
+			if (palabra[i] == toupper(caracter))
 			{
-				palabra_jugar[i] = caracter;
+				palabra_jugar[i] = toupper(caracter);
 				letra_encontrada = true;
 			}
 		}
@@ -84,7 +82,8 @@ void IniciarJuego(string palabra)
 		{
 			FinJuego = true;
 		}
-		cout << "Palabra: " << palabra_jugar << " Intentos " << Intentos << endl;
+
+		MostrarAhorcado(palabra_jugar, LetrasEscondidas, Intentos);
 	}
 
 	if (palabra_jugar == palabra)
@@ -94,6 +93,50 @@ void IniciarJuego(string palabra)
 	else
 	{
 		cout << "Perdedor !!!!!!" << endl;
+	}
+}
+
+void MostrarAhorcado(string palabra_jugar, int TotalIntentos, int IntentosRestantes)
+{
+	borrarpantalla();
+	CrearTitulo("Ahorcado");
+	cout << "	" << palabra_jugar << endl
+		 << endl
+		 << "Intentos " << IntentosRestantes << endl
+		 << endl;
+
+	cout << "	" << palabra_jugar << endl
+		 << endl;
+
+	cout << "   [-][-][-][-][-][-][-][-][-]" << endl;
+	cout << "   [-]                      |" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+	cout << "   [-]" << endl;
+
+	if (TotalIntentos == IntentosRestantes)
+	{
+		cout << "   [-]                      O" << endl;
+		cout << "   [-]                    _/|\\_" << endl;
+		cout << "   [-]                      |" << endl;
+		cout << "[-][-][-]                 _/ \\_" << endl;
+	}
+	else
+	{
+		cout << "   [-]              x        O" << endl;
+		cout << "   [-]              x      _/|\\_" << endl;
+		cout << "   [-]              x        |" << endl;
+		cout << "[-][-][-]           x      _/ \\_" << endl;
 	}
 }
 
@@ -119,8 +162,6 @@ void Jugar()
 	{
 		// Elegir parabra aleatoriamente
 		int random = 1 + (rand() % ListaPalabras.size() - 1);
-		//	cout << "Random: " << random << endl;
-		//		cout << "Palabra: " << ListaPalabras[random] << endl;
 		IniciarJuego(ListaPalabras[random]);
 	}
 	else
@@ -141,15 +182,13 @@ void Presentacion()
 // Lee un archivo de texto y lo guarda en una matriz
 string *LeerArchivo()
 {
-	cout << "Leer archivo palabras.txt" << endl;
+	// cout << "Leer archivo palabras.txt" << endl;
 	int numlinea = 0;
 	ifstream archivo("palabras.txt");
 	string linea;
 	string *matriz = new string[100];
 	while (getline(archivo, linea))
 	{
-		// cout << "**** " + linea + " *****" << endl;
-		//  std::istringstream iss(linea);
 		matriz[numlinea] = linea;
 		numlinea++;
 	}
@@ -167,34 +206,6 @@ string rellenartexto(string texto_inicial, int longitud_final)
 	}
 	salida = (salida.substr(0, longitud_final)) + " ";
 	return salida;
-}
-
-void imprimirlibros(string matriz[3][4])
-{
-	int i;
-	i = 1;
-	// t�tulo, autor, a�o y color del libro
-	cout << endl;
-	cout << rellenartexto("#", 2) << rellenartexto("TITULO", 20) << rellenartexto("AUTOR", 20) << rellenartexto("ANIO", 10) << rellenartexto("COLOR", 15) << endl;
-	for (i = 1; i <= 3; i++)
-	{
-		cout << rellenartexto(convertiratexto(i), 2) << rellenartexto(matriz[i - 1][0], 20) << rellenartexto(matriz[i - 1][1], 20) << rellenartexto(matriz[i - 1][2], 10) << rellenartexto(matriz[i - 1][3], 15) << endl;
-	}
-	cout << endl;
-}
-
-void imprimirtrabajadores(string matriz[3][5])
-{
-	int i;
-	i = 1;
-	// Nombre y apellido, edad, ciudad, cargo, salario
-	cout << endl;
-	cout << rellenartexto("#", 2) << rellenartexto("NOMBRE", 20) << rellenartexto("EDAD", 10) << rellenartexto("CIUDAD", 10) << rellenartexto("CARGO", 15) << rellenartexto("SALARIO", 10) << endl;
-	for (i = 1; i <= 3; i++)
-	{
-		cout << rellenartexto(convertiratexto(i), 2) << rellenartexto(matriz[i - 1][0], 20) << rellenartexto(matriz[i - 1][1], 10) << rellenartexto(matriz[i - 1][2], 10) << rellenartexto(matriz[i - 1][3], 15) << rellenartexto(matriz[i - 1][4], 10) << endl;
-	}
-	cout << endl;
 }
 
 void presionarteclacontinuar()
@@ -222,100 +233,13 @@ void dibujarasteriscos(float longitud_texto)
 	cout << linea << endl;
 }
 
-void creartitulo(string texto)
+void CrearTitulo(string texto)
 {
 	int cantidadcaracteres;
 	cantidadcaracteres = texto.size();
 	dibujarasteriscos(cantidadcaracteres);
 	cout << "**** " + texto + " *****" << endl;
 	dibujarasteriscos(cantidadcaracteres);
-}
-
-void reglibros()
-{
-	int c;
-	int f;
-	int i;
-	string libros[3][4];
-	string titulo;
-	titulo = "REGISTRO DE LIBROS";
-	f = 3;
-	c = 4;
-	cout << endl;
-	borrarpantalla();
-
-	for (i = 1; i <= f; i++)
-	{
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese TITULO del libro " << i << endl;
-		imprimirlibros(libros);
-		cin >> libros[i - 1][0];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese AUTOR del libro " << i << endl;
-		imprimirlibros(libros);
-		cin >> libros[i - 1][1];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese ANIO del libro " << i << endl;
-		imprimirlibros(libros);
-		cin >> libros[i - 1][2];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese COLOR del libro " << i << endl;
-		imprimirlibros(libros);
-		cin >> libros[i - 1][3];
-	}
-	borrarpantalla();
-	creartitulo(titulo);
-	imprimirlibros(libros);
-	presionarteclacontinuar();
-}
-
-void regtrabajadores()
-{
-	int c;
-	int f;
-	int i;
-	string titulo;
-	string trabajadores[3][5];
-	titulo = "REGISTRO DE TRABAJADORES";
-	f = 3;
-	c = 5;
-	borrarpantalla();
-	for (i = 1; i <= f; i++)
-	{
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese NOMBRE del trabajador " << i << endl;
-		imprimirtrabajadores(trabajadores);
-		cin >> trabajadores[i - 1][0];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese EDAD del trabajador " << i << endl;
-		imprimirtrabajadores(trabajadores);
-		cin >> trabajadores[i - 1][1];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese CIUDAD del trabajador " << i << endl;
-		imprimirtrabajadores(trabajadores);
-		cin >> trabajadores[i - 1][2];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese CARGO del trabajador " << i << endl;
-		imprimirtrabajadores(trabajadores);
-		cin >> trabajadores[i - 1][3];
-		borrarpantalla();
-		creartitulo(titulo);
-		cout << "Ingrese SALARIO del trabajador " << i << endl;
-		imprimirtrabajadores(trabajadores);
-		cin >> trabajadores[i - 1][4];
-	}
-	borrarpantalla();
-	creartitulo(titulo);
-	imprimirtrabajadores(trabajadores);
-	presionarteclacontinuar();
 }
 
 void menuprincipal()
